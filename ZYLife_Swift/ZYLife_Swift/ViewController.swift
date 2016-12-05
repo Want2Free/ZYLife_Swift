31077c6ec5504e68179844f3eea0d6a5e6bc7b62
黄江 <hjs8906@163.com>
2016-12-05 18:43:45 +0800

    初始化项目。

---
+++ ZYLife_Swift/ZYLife_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZYLife_Swift
+//
+//  Created by Jason on 16/12/5.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
